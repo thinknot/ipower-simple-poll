aa15ad9241b582b0050d5d45ffd0df548e106f72
cc <inventerix@gmail.com>
2020-02-18 18:56:15 -0700

    remove local dev host

--- tests/01_basic_test.py
+++ tests/01_basic_test.py
@@ -9,12 +9,12 @@ from lxml import etree
 
 
 def test_get_instant_check_status_code_equals_200():
-    response = requests.get('http://10.0.0.47/cgi-bin/egauge?inst&tot')
+    response = requests.get('http://egauge45930.d.egauge.net/cgi-bin/egauge?inst&tot')
     assert response.status_code == 200
 
 
 def test_get_instant_check_content_type_equals_xml():
-    response = requests.get('http://10.0.0.47/cgi-bin/egauge?inst&tot')
+    response = requests.get('http://egauge45930.d.egauge.net/cgi-bin/egauge?inst&tot')
     assert response.headers['Content-Type'] == 'text/xml; charset=utf-8'
 
 
@@ -22,25 +22,25 @@ def test_get_instant_check_content_xml_is_well_formed():
     parser = make_parser()
     parser.setContentHandler(ContentHandler())
     try:
-        parser.parse('http://10.0.0.47/cgi-bin/egauge?inst&tot')
+        parser.parse('http://egauge45930.d.egauge.net/cgi-bin/egauge?inst&tot')
     except Exception:
         assert False
 
 
 def test_get_instant_check_xml_syntax_is_valid():
     try:
-        doc = etree.parse('http://10.0.0.47/cgi-bin/egauge?inst&tot')
+        doc = etree.parse('http://egauge45930.d.egauge.net/cgi-bin/egauge?inst&tot')
     except etree.XMLSyntaxError:
         assert False
 
 
 def test_get_history_check_status_code_equals_200():
-        response = requests.get('http://10.0.0.47/cgi-bin/egauge-show')
+        response = requests.get('http://egauge45930.d.egauge.net/cgi-bin/egauge-show')
         assert response.status_code == 200
 
 
 def test_get_history_check_content_type_equals_xml():
-        response = requests.get('http://10.0.0.47/cgi-bin/egauge-show')
+        response = requests.get('http://egauge45930.d.egauge.net/cgi-bin/egauge-show')
         assert response.headers['Content-Type'] == 'text/xml; charset=utf-8'
 
 
@@ -48,14 +48,14 @@ def test_get_history_check_content_xml_is_well_formed():
         parser = make_parser()
         parser.setContentHandler(ContentHandler())
         try:
-            parser.parse('http://10.0.0.47/cgi-bin/egauge-show')
+            parser.parse('http://egauge45930.d.egauge.net/cgi-bin/egauge-show')
         except Exception:
             assert False
 
 
 def test_get_history_check_xml_syntax_is_valid():
         try:
-            doc = etree.parse('http://10.0.0.47/cgi-bin/egauge-show')
+            doc = etree.parse('http://egauge45930.d.egauge.net/cgi-bin/egauge-show')
         except etree.XMLSyntaxError:
             assert False
 
